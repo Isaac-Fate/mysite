9c8cb50d9cf33f9f59002fea9c35ea71dfa80553
isaac-fei <isaac.omega.fei@gmail.com>
2024-11-14 01:10:13 +0800

    used unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,26 @@ import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
 
+// @ts-ignore
+import { imageService } from "@unpic/astro/service";
+
 import mdx from "@astrojs/mdx";
 
 import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: imageService({
+      // This can usually be auto-detected
+      // fallbackService: "netlify",
+
+      placeholder: "blurhash",
+
+      // This is the default
+      layout: "constrained",
+    }),
+  },
   integrations: [
     react(),
     tailwind({
